bb00eda4ee0fc171f74d730d7ceee8dae29983f0
Alan Rehfeldt <alanrehfeldt@MacBook-Pro-de-Alan.local>
2023-09-07 13:09:59 -0300

    ignite-feed in typescript

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,29 @@
+import { PencilLine } from 'phosphor-react'
+
+import styles from './Sidebar.module.css'
+import { Avatar } from './Avatar'
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img
+                className={styles.cover} 
+                src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZGV2ZWxvcGVyfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=50" 
+            />
+
+            <div className={styles.profile}>
+                <Avatar src="https://github.com/AlanRehfeldt.png" />
+
+                <strong>Alan Rehfeldt</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    <PencilLine size={20} />
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    )
+}
